a7810f5dcff3c6665ee9ef9774c9ab9f3cd802cf
David Soto <davidsoto06@ciencias.unam.mx>
2026-04-01 17:53:23 -0600

    Prueba de id3, por el momento solo se como hacerlo con una cancion a la vez, agregue un album de ejemplo

--- music-db/src/main.rs
+++ music-db/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use id3::{Tag, TagLike};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let tag = Tag::read_from_path("./wetdream/stop calling.mp3")?;
+
+    if let Some(artist) = tag.artist() {
+        println!("Artista: {}", artist);
+    }
+    if let Some(title) = tag.title() {
+        println!("Titulo: {}", title);
+    }
+    if let Some(album) = tag.album() {
+        println!("Album: {}", album);
+    }
+    if let Some(track) = tag.track(){
+        println!("Numero de pista: {}", track);
+    }
+    Ok(())
 }
