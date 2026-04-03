d2158459443093f696c2fa1714717096460730ba
David Soto <davidsoto06@ciencias.unam.mx>
2026-04-02 21:08:05 -0600

    Ya funciona el minero en una carpeta completa, despues como hacer el sql

--- music-db/src/main.rs
+++ music-db/src/main.rs
@@ -1,24 +1,32 @@
 use id3::{Tag, TagLike};
-use walkdir::WalkDir;
+use walkdir::{WalkDir, DirEntry};
+
+fn es_mp3(entry: &DirEntry) -> bool {
+    entry.file_name().to_str().map(|x| x.ends_with(".mp3")).unwrap_or(false)
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let tag = Tag::read_from_path("./wetdream/stop calling.mp3")?;
+    let directorio = WalkDir::new("wetdream").into_iter().filter_map(|x| x.ok());
+    for entry in directorio {
+        if es_mp3(&entry){
+            let path = entry.path();
+            let tag = Tag::read_from_path(path)?;
 
-    if let Some(artist) = tag.artist() {
-        println!("Artista: {}", artist);
-    }
-    if let Some(title) = tag.title() {
-        println!("Titulo: {}", title);
-    }
-    if let Some(album) = tag.album() {
-        println!("Album: {}", album);
-    }
-    if let Some(track) = tag.track(){
-        println!("Numero de pista: {}", track);
-    }
-    println!("\n\n\n\n");
-    for entry in WalkDir::new("wetdream") {
-        println!("{}", entry?.path().display());
+            if let Some(artist) = tag.artist() {
+                println!("Artista: {}", artist);
+            }
+            if let Some(title) = tag.title() {
+                println!("Titulo: {}", title);
+            }
+            if let Some(album) = tag.album() {
+                println!("Album: {}", album);
+            }
+            if let Some(track) = tag.track(){
+                println!("Numero de pista: {}", track);
+            }
+            println!("{}", path.display());
+            print!("\n");
+        }
     }
     Ok(())
 }
