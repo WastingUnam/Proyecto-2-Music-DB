ae06bdccfcbf43a4cee430eea096eeba9c24b2fe
David Soto <davidsoto06@ciencias.unam.mx>
2026-04-10 14:12:04 -0600

    Ya regresa una lista de cacniones, y tengo los modulos vacios para el DAO

--- music-db/src/minero/minero.rs
+++ music-db/src/minero/minero.rs
@@ -1,24 +1,27 @@
-use walkdir::{WalkDir, DirEntry};
 use super::audio;
+use super::audio::Cancion;
+use walkdir::{DirEntry, WalkDir};
 
 fn es_mp3(entry: &DirEntry) -> bool {
-	entry.file_name().to_str()
-	.map(|x| x.ends_with(".mp3"))
-	.unwrap_or(false)
+    entry
+        .file_name()
+        .to_str()
+        .map(|x| x.ends_with(".mp3"))
+        .unwrap_or(false)
 }
 
-pub fn mina(ruta: &str) -> Result<(), Box<dyn std::error::Error>> {
-	let directorio = WalkDir::new(ruta).into_iter().filter_map(|x| x.ok());
+pub fn mina(ruta: &str) -> Vec<Cancion> {
+    let directorio = WalkDir::new(ruta).into_iter().filter_map(|x| x.ok());
+    let mut canciones: Vec<Cancion> = Vec::new();
 
-	for entry in directorio {
-
-		if !entry.file_type().is_file() {
-			continue;
-		}
-		// Pasamos el archivo especifico, asi funcionan los que buscan los metadatos.
-		if es_mp3(&entry) {
-			let _ = audio::mp3(entry.path());
-		}
-	}
-	Ok(())
+    for entry in directorio {
+        if !entry.file_type().is_file() {
+            continue;
+        }
+        // Pasamos el archivo especifico, asi funcionan los que buscan los metadatos.
+        if es_mp3(&entry) {
+            canciones.push(audio::mp3(entry.path()))
+        }
+    }
+    canciones
 }
